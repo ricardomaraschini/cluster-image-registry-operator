36bda223c016ad64711f2165c22c6542b6b74b83
Ricardo Maraschini <rmarasch@redhat.com>
2019-09-25 11:38:30 +0200

    Adding day 2 ops tests for BareMetal installs.
    Added a test that migrates the operator status from Removed to Managed,
    setting the Storage to EmptyDir. Test verifies if Registry is available
    after the change.

--- test/e2e/baremetal_test.go
+++ test/e2e/baremetal_test.go
@@ -3,43 +3,72 @@ package e2e
 import (
 	"testing"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"github.com/openshift/cluster-image-registry-operator/pkg/apis/imageregistry/v1"
+	"github.com/openshift/cluster-image-registry-operator/test/framework"
 
 	configapiv1 "github.com/openshift/api/config/v1"
-	"github.com/openshift/cluster-image-registry-operator/test/framework"
+	operatorapi "github.com/openshift/api/operator/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestBaremetalDefaults(t *testing.T) {
 	client := framework.MustNewClientset(t, nil)
 
-	infrastructureConfig, err := client.Infrastructures().Get("cluster", metav1.GetOptions{})
+	infra, err := client.Infrastructures().Get("cluster", metav1.GetOptions{})
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if infrastructureConfig.Status.PlatformStatus.Type != configapiv1.BareMetalPlatformType {
+	if infra.Status.PlatformStatus.Type != configapiv1.BareMetalPlatformType {
 		t.Skip("skipping on non-BareMetal platform")
 	}
 
 	// Start of the meaningful part
 	defer framework.MustRemoveImageRegistry(t, client)
+
 	framework.MustDeployImageRegistry(t, client, nil)
 	cr := framework.MustEnsureImageRegistryIsProcessed(t, client)
+	framework.MustEnsureClusterOperatorStatusIsNormal(t, client)
 
-	clusterOperator := framework.MustEnsureClusterOperatorStatusIsSet(t, client)
-	for _, cond := range clusterOperator.Status.Conditions {
-		switch cond.Type {
-		case configapiv1.OperatorAvailable:
-			if cond.Status != configapiv1.ConditionTrue {
-				t.Errorf("expected clusteroperator to report Available=%s, got %s", configapiv1.ConditionTrue, cond.Status)
-			}
-			if cond.Reason != "Removed" {
-				t.Errorf("expected reason to be 'Removed', got %s", cond.Reason)
-			}
-		case configapiv1.OperatorDegraded:
-			if cond.Status != configapiv1.ConditionFalse {
-				t.Errorf("the operator is not expected to be degraded, got: %s", cond.Status)
-			}
-		}
+	conds := framework.GetImageRegistryConditions(cr)
+	if conds.Available.Reason() != "Removed" {
+		t.Errorf("exp Available reason: Removed, got %s", conds.Available.Reason())
 	}
+	if conds.Degraded.Reason() != "Removed" {
+		t.Errorf("exp Degraded reason: Removed, got %s", conds.Degraded.Reason())
+	}
+	if conds.Progressing.Reason() != "Removed" {
+		t.Errorf("exp Progressing reason: Removed, got %s", conds.Progressing.Reason())
+	}
+}
+
+func TestBaremetalDay2Operations(t *testing.T) {
+	client := framework.MustNewClientset(t, nil)
+
+	infra, err := client.Infrastructures().Get("cluster", metav1.GetOptions{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if infra.Status.PlatformStatus.Type != configapiv1.BareMetalPlatformType {
+		t.Skip("skipping on non-BareMetal platform")
+	}
+
+	// Start of the meaningful part
+	defer framework.MustRemoveImageRegistry(t, client)
+
+	framework.MustDeployImageRegistry(t, client, nil)
+	cf := framework.MustEnsureImageRegistryIsProcessed(t, client)
+
+	// Set registry to managed with empty dir storage engine.
+	cf.Spec.ManagementState = operatorapi.Managed
+	cf.Spec.Storage.EmptyDir = &v1.ImageRegistryConfigStorageEmptyDir{}
+	if _, err := client.Configs().Update(cf); err != nil {
+		t.Errorf("error updating config: %v", err)
+	}
+
+	framework.MustEnsureOperatorIsNotHotLooping(t, client)
+	framework.MustEnsureImageRegistryIsAvailable(t, client)
+	framework.MustEnsureInternalRegistryHostnameIsSet(t, client)
+	framework.MustEnsureClusterOperatorStatusIsNormal(t, client)
 }
