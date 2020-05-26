40b5c2ea0aeb8eb2e0f5a924029e67bbf8ec40a8
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2020-05-26 13:00:57 +0200

    Enabling operator logging controller
    Enabled a controller to take care of operator log level. Old logging
    field has been deprecated in favour of logLevel (part of OperatorSpec
    struct).

--- pkg/resource/podtemplatespec.go
+++ pkg/resource/podtemplatespec.go
@@ -16,22 +16,35 @@ import (
 
 	configapiv1 "github.com/openshift/api/config/v1"
 	v1 "github.com/openshift/api/imageregistry/v1"
+	operatorapiv1 "github.com/openshift/api/operator/v1"
 	configlisters "github.com/openshift/client-go/config/listers/config/v1"
 
 	"github.com/openshift/cluster-image-registry-operator/pkg/defaults"
 	"github.com/openshift/cluster-image-registry-operator/pkg/storage"
 )
 
+// generateLogLevel returns the appropriate operand log level according to user
+// provided configuration. LogLevel takes precedence over deprecated Logging
+// field.
 func generateLogLevel(cr *v1.Config) string {
+	if cr.Spec.LogLevel == "" {
+		switch cr.Spec.Logging {
+		case 0:
+			return "error"
+		case 1:
+			return "warn"
+		case 2, 3:
+			return "info"
+		}
+		return "debug"
+	}
+
 	switch cr.Spec.LogLevel {
-	case 0:
-		return "error"
-	case 1:
-		return "warn"
-	case 2, 3:
+	case operatorapiv1.Debug, operatorapiv1.Trace, operatorapiv1.TraceAll:
+		return "debug"
+	default:
 		return "info"
 	}
-	return "debug"
 }
 
 func generateLivenessProbeConfig() *corev1.Probe {
